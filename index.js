6063b274660ddd73bf368fac260b146103e0dd4b
scherpablo <scherpablo@gmail.com>
2023-11-15 12:23:15 -0300

    render.yaml

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./src/server")
 const { conn } = require('./src/db.js')
-const PORT = 3001
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 3001
+
+dotenv.config();
 
 conn.sync({ force: true }).then(() => {
 server.listen(PORT, () => {
