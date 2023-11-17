9c337e02c1a43e478223384229d30b527f15e804
scherpablo <scherpablo@gmail.com>
2023-11-16 22:31:42 -0300

    update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3001
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
